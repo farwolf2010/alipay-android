a64ac60912f47d5b66e280637c072e8e0f8ff57a
郑江荣 <362675035@qq.com>
2018-11-19 15:41:02 +0800

    udpate

---
+++ build/generated/source/buildConfig/debug/test/farwolf/com/alipay/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package test.farwolf.com.alipay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "test.farwolf.com.alipay";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
